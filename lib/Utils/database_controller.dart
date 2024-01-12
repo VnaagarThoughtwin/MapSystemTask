111781034ee2e5954f58d95c52447edef02edf89
aniket <anuj@thoughtwin.com>
2024-01-12 13:35:44 +0530

    Create DB

--- lib/Utils/database_controller.dart
+++ lib/Utils/database_controller.dart
@@ -1,26 +1,25 @@
 
-import 'package:system_task_on_map/models/timer_model.dart';
-
+import '../models/CustomMarker.dart';
 import 'database_provider.dart';
 
 
 class DatabaseController{
   final dbClient = DatabaseProvider.dbProvider;
 
-  Future<int> createTODO(TimerModel? todo) async {
+  Future<int> createMarkers(CustomMarker? todo) async {
     final db = await dbClient.db;
     int result = await db.insert("timerTable", todo!.toJSON());
     return result;
   }
 
-  Future<List<TimerModel>> getAllTODOS({List<String>? columns}) async {
+  Future<List<CustomMarker>> getAllMarkers({List<String>? columns}) async {
     final db = await dbClient.db;
     var result = await db.query("timerTable",columns: columns);
-    List<TimerModel> todos = result.isNotEmpty ? result.map((item) => TimerModel.fromJSON(item)).toList() : [];
+    List<CustomMarker> todos = result.isNotEmpty ? result.map((item) => CustomMarker.fromJSON(item)).toList() : [];
     return todos;
   }
 
-  Future<int> updateTODO(TimerModel? todo) async {
+  Future<int> updateTODO(CustomMarker? todo) async {
     final db = await dbClient.db;
     var result = await db.update("todoTable", todo!.toJSON(),where: "id = ?", whereArgs: [todo.id]);
     return result;
--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -6,16 +6,29 @@ import 'package:provider/provider.dart';
 import 'package:system_task_on_map/controllers/marker.dart';
 import 'package:system_task_on_map/models/CustomMarker.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   HomeView({super.key});
-
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+
+  @override
+  void initState() {
+    // Provider.of<MakeMarker>(context, listen: false)
+    //     .getMarkers();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,29 +37,30 @@ class HomeView extends StatelessWidget {
         builder: (BuildContext context, MakeMarker value, Widget? child) {
           return GoogleMap(
             mapType: MapType.hybrid,
-            initialCameraPosition: _kGooglePlex,
+            initialCameraPosition: HomeView._kGooglePlex,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
             markers: value.markers.map((marker) {
               return Marker(
-                markerId: MarkerId(marker.position.toString()),
-                position: marker.position,
+                markerId: MarkerId(marker.id.toString()),
+                position: LatLng(marker.latitude??0,marker.longitude??0),
                 infoWindow: InfoWindow(
                   title: marker.label,
                   snippet: "Tap on it to remove",
                   onTap: () => Provider.of<MakeMarker>(context, listen: false)
-                      .removeMarker(marker.id),
+                      .removeMarker(marker.id??0),
                 ),
               );
             }).toSet(),
             onTap: (argument) {
               return Provider.of<MakeMarker>(context, listen: false).addMarker(
                   CustomMarker(
-                      position: argument,
+                      latitude: argument.latitude,
+                      longitude: argument.longitude,
                       label:
                           "Lat: ${argument.latitude}, Long : ${argument.longitude}",
-                      id: UniqueKey().hashCode));
+                      ));
             },
           );
         },
